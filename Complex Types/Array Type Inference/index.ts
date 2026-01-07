4aabafa948e388691cbfe764507e8bc49d03d21a
LuluH19 <luca.delarue@efrei.net>
2026-01-08 00:25:32 +0100

    Array Type Inference

---
+++ Complex Types/Array Type Inference/index.ts
@@ -0,0 +1,22 @@
+// Don't change this part:
+let dogTup: [string, string, string, string] = [
+  "dog",
+  "brown fur",
+  "curly tail",
+  "sad eyes",
+];
+
+// Your code goes here:
+let myArr = dogTup.concat([]);
+
+// 2nd question:
+// Don't change this part:
+let dogTup: [string, string, string, string] = [
+  "dog",
+  "brown fur",
+  "curly tail",
+  "sad eyes",
+];
+
+// Your code goes here:
+let myArr[50] = 'not a dog';
